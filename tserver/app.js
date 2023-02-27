4cbc7c6bbf945d915712079c84cba1f5c1cdbeaa
hhshiblu <hhshiblu5555@gmail.com>
2023-02-28 00:29:28 +0600

    j

--- tserver/app.js
+++ tserver/app.js
@@ -1,53 +1,65 @@
-const express = require('express');
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', true)
+const express = require("express");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://0.0.0.0:27017/todolist')
-.then(() => {
-  console.log('Connected to MongoDB');
-}).catch((err) => {
-  console.error(err);
-});
+mongoose
+  .connect("mongodb://0.0.0.0:27017/todolist")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const taskSchema = new mongoose.Schema({
   title: String,
   description: String,
 });
 
-const Task = mongoose.model('Task', taskSchema);
 
-app.get('/tasks', async (req, res) => {
+const Task = mongoose.model("Task", taskSchema);
+
+
+app.get("/tasks", async (req, res) => {
   const tasks = await Task.find();
   res.json(tasks);
 });
 
-app.post('/tasks', async (req, res) => {
-  const { title, description } = req.body;
-  const task = new Task({ title, description });
-  await task.save();
-  res.json(task);
+app.post("/tasks", async (req, res) => {
+  try{
+    const { title, description } = req.body;
+    const task = new Task({ title, description });
+   
+    await task.save();
+   
+    res.status(201).json(task);
+  }
+  catch(err){
+    console.log(err)
+  }
+  
 });
 
-app.put('/tasks/:id', async (req, res) => {
+app.put("/tasks/:id", async (req, res) => {
   const { id } = req.params;
   const { title, description } = req.body;
   const task = await Task.findByIdAndUpdate(id, { title, description }, { new: true });
   res.json(task);
 });
 
-app.delete('/tasks/:id', async (req, res) => {
+app.delete("/tasks/:id", async (req, res) => {
   const { id } = req.params;
   await Task.findByIdAndDelete(id);
   res.sendStatus(204);
 });
 
 app.listen(5000, () => {
-  console.log('Server started on port 5000');
+  console.log("Server started on port 5000");
 });
